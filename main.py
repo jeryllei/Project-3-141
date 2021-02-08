1ce51294183af82abe6a59063ae78a744f3627c3
jeryllei <70560506+jeryllei@users.noreply.github.com>
2021-02-07 22:19:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ if __name__ == '__main__':
     userInput = ''
     while True:
         userInput = input('Enter a query (type in \'quit\' to exit): ')
-        quitWords = ['quit', 'Quit', 'QUIT']
-        if userInput in quitWords:
+        userInput = userInput.lower()
+        if userInput == 'quit':
             break
-        
 
