dff94a4891a9bdbc9180cb308b84d032bc896138
jeryllei <70560506+jeryllei@users.noreply.github.com>
2021-02-08 23:25:18 -0800

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -8,6 +8,11 @@ def loadBookkeeping():
         data = json.load(json_file)
     return data
 
+def loadBookkeepingTest():
+    with open('bookkeeperTEST.json', 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
 class Index:
     def __init__(self):
         # Structure: {token: [{docID: '0/100', tfidf: '1'}]}
@@ -25,7 +30,33 @@ class Index:
     def addIndexEntry(self, token, docid, tf_idf):
         self.index[token].append(dict(docID=docid, tfidf=tf_idf))
 
+
+# Structure of DB:
+'''
+{
+    '_id': 'token',
+    'postings': [{'docID': '0/11', 'frequency': 10, 'tf_idf': 2, 'tags':['h1', 'h2', 'title', 'strong']}, {'docID': '0/34', 'frequency': 10, 'tf_idf': 4, 'tags':[]}]
+}
+'''
+def constructIndex(data, collection):
+    for docID, url in data.items():
+        directory = 'WEBPAGES_RAW/' + docID
+        with open(directory, 'r', encoding='utf-8') as html_page:
+            soup = BeautifulSoup(html_page, 'html.parser')
+            page_text = soup.getText()
+            page_title = soup.head
+            if page_title != None and soup.head.title != None:
+                page_title = str(soup.head.title.text).strip()
+            else:
+                page_title = 'No title'
+            collection.insert_one({'docID': docID, 'url': url, 'page_title': page_title})
+            print('Page indexed (ID: %s): %s', (docID, url))
+    return
+
 if __name__ == "__main__":
+    myClient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mydb = myClient['myDatabase']
+    myCollection = mydb['testIndex']
+
     myData = loadBookkeeping()
-    for docID, url in myData.items():
-        pass
+    constructIndex(myData, myCollection)
