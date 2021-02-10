d64b954476ef9722dccc1afd2db418b3cb146f5c
jeryllei <70560506+jeryllei@users.noreply.github.com>
2021-02-10 15:31:18 -0800

    Begin working on user input

--- user_input.py
+++ user_input.py
@@ -1,19 +1,34 @@
-from nltk.stem import WordNetLemmatizer
+import pymongo
 import json
 
-# Loads in the bookkeeping.json as a dictionary.
 def loadBookkeeping():
     with open('WEBPAGES_RAW/bookkeeping.json', 'r') as json_file:
         data = json.load(json_file)
     return data
 
+# Structure of DB:
+'''
+{
+    '_id': 'token',
+    'postings': [{'docID': '0/11', 'frequency': 10, 'tf_idf': 2, 'tags':['h1', 'h2', 'title', 'strong']}, {'docID': '0/34', 'frequency': 10, 'tf_idf': 4, 'tags':[]}]
+}
+'''
 if __name__ == '__main__':
+    myClient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mydb = myClient['myDatabase']
+    myCollection = mydb['testIndex']
 
+    docIDs = loadBookkeeping()
     
     while True:
         userInput = input('Enter a query (type in \'quit\' to exit): ')
         userInput = userInput.lower()
         if userInput == 'quit':
             break
+        else:
+            if myCollection.find_one({'_id': userInput}) != None:
+                pass
+            else:
+                print('Search query returned no results.')
     
 
