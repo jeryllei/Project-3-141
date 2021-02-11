b353386dd814c509e3c705121eb9c8dc33ed1e0f
jeryllei <70560506+jeryllei@users.noreply.github.com>
2021-02-11 13:51:41 -0800

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -15,24 +15,6 @@ def loadBookkeepingTest():
         data = json.load(json_file)
     return data
 
-class Index:
-    def __init__(self):
-        # Structure: {token: [{docID: '0/100', tfidf: '1'}]}
-        self.index = defaultdict(list)
-
-    # Returns the index itself.
-    def getIndex(self):
-        return self.index
-
-    # Returns a list of documents for a given token.
-    def getIndexEntry(self, token):
-        return self.index[token]
-
-    # Adds an entry to the Index, creates a new entry automatically if the token didn't already exist.
-    def addIndexEntry(self, token, docid, tf_idf):
-        self.index[token].append(dict(docID=docid, tfidf=tf_idf))
-
-
 # Structure of DB:
 '''
 {
