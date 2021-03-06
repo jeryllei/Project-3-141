308b00a86a9ca0357673193ae0af77c3d41d3077
jeryllei <70560506+jeryllei@users.noreply.github.com>
2021-03-05 18:09:20 -0800

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -7,6 +7,30 @@ def loadBookkeeping():
         data = json.load(json_file)
     return data
 
+def cosineScoring(userInput, collection, docIDs):
+    # {docID: sum(cos(q, d)), }
+    ranked_results = defaultdict(float)
+    for query in userInput:
+        queryResults = collection.find_one({'_id': query})
+    pass
+
+# Function to print out results of the search query. Limited to 20 results, extra results are truncated.
+def printResults(rankedDocIDs, docIDs):
+    if len(rankedDocIDs) > 0:
+        i = 1
+        if len(rankedDocIDs) <= 20:
+            for docID in rankedDocIDs:
+                print(f'Result {i} of {len(ranked_IDs)}\tDocID: {docID}\tURL: {docIDs[docID]}')
+                i += 1
+        else:
+            for docID in rankedDocIDs:
+                print(f'Result {i} of {len(ranked_IDs)}\tDocID: {docID}\tURL: {docIDs[docID]}')
+                i += 1
+                if i > 20:
+                    break
+    else:
+        print(f'Search query returned no results.\n')
+
 # Structure of DB:
 '''
 {
@@ -17,11 +41,12 @@ def loadBookkeeping():
 if __name__ == '__main__':
     myClient = pymongo.MongoClient('mongodb://localhost:27017/')
     mydb = myClient['myDatabase']
-    myCollection = mydb['testIndex']
+    myCollection = mydb['oneGramIndex']
 
     docIDs = loadBookkeeping()
     
     while True:
+        # Gets the user input and lower cases it. Index is also constructed with lower cased entries.
         userInput = input('Enter a query (type in \'quit\' to exit): ').lower()
         if userInput == 'quit':
             print('Exiting program...')
@@ -33,7 +58,7 @@ if __name__ == '__main__':
             # ranked_results is a dictionary of docID keys and sum of tf-idf scores values.
             # Results that contain more parts of the query will be handled already by how a defaultdict functions.
             ranked_results = defaultdict(float)
-            # ranked_IDs is a list of document IDs, in descending order of scores.
+            # ranked_IDs is a list of document IDs, to be sorted in descending order of scores.
             ranked_IDs = []
 
             # For each word in the user input, it finds if it is in the index and collects the documents and tf-idf scores associated with them into ranked_results.
@@ -50,14 +75,15 @@ if __name__ == '__main__':
             # Modified code from: https://stackoverflow.com/a/613218
             ranked_IDs = [docID for docID, score in sorted(ranked_results.items(), key=lambda item: item[1], reverse=True)]
 
-            i = 1
+            '''i = 1
             if len(ranked_IDs) > 0:
                 for docID in ranked_IDs:
                     print(f'Result {i} of {len(ranked_IDs)}\tDocID: {docID}\tURL: {docIDs[docID]}')
                     i += 1
                 print(f'End of query results. {len(ranked_IDs)} total result(s) found.\n')
             else:
-                print('Search query returned no results.\n')
+                print('Search query returned no results.\n')'''
+            printResults(ranked_IDs, docIDs)
 
         # Handles single word inputs.
         else:
@@ -70,12 +96,17 @@ if __name__ == '__main__':
                 for post in results:
                     ranked_results[post['docID']] += post['tf_idf']
                 ranked_IDs = [docID for docID, score in sorted(ranked_results.items(), key=lambda item: item[1], reverse=True)]
+
                 i = 1
                 if len(ranked_IDs) > 0:
                     for docID in ranked_IDs:
                         print(f'Result {i} of {len(ranked_IDs)}\tDocID: {docID}\tURL: {docIDs[docID]}')
+                        print(f'TF-IDF Score: {ranked_results[docID]}')
                         i += 1
+                        if i > 20:
+                            break
                     print(f'End of query results. {len(ranked_IDs)} total result(s) found. \n')
+                
             else:
                 print('Search query returned no results.\n')
     
